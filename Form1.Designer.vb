5c02764ce14ad8b80d9d0bcb4ea582606d11db42
413C5 <124840787+413C5@users.noreply.github.com>
2023-03-16 14:44:46 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnReconocer = New System.Windows.Forms.Button()
+        Me.btnVisibilidad = New System.Windows.Forms.Button()
+        Me.btnPrevencion = New System.Windows.Forms.Button()
+        Me.btnEstetico = New System.Windows.Forms.Button()
+        Me.btnControl = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtTexto = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPalabra = New System.Windows.Forms.TextBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(234, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(247, 59)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Heuristicas"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnReconocer
+        '
+        Me.btnReconocer.Location = New System.Drawing.Point(34, 67)
+        Me.btnReconocer.Name = "btnReconocer"
+        Me.btnReconocer.Size = New System.Drawing.Size(635, 29)
+        Me.btnReconocer.TabIndex = 1
+        Me.btnReconocer.Text = "&Reconocer en Vez de Recordar"
+        Me.btnReconocer.UseVisualStyleBackColor = True
+        '
+        'btnVisibilidad
+        '
+        Me.btnVisibilidad.Location = New System.Drawing.Point(34, 111)
+        Me.btnVisibilidad.Name = "btnVisibilidad"
+        Me.btnVisibilidad.Size = New System.Drawing.Size(635, 29)
+        Me.btnVisibilidad.TabIndex = 2
+        Me.btnVisibilidad.Text = "&Visibilidad de Estado del Sistema"
+        Me.btnVisibilidad.UseVisualStyleBackColor = True
+        '
+        'btnPrevencion
+        '
+        Me.btnPrevencion.Location = New System.Drawing.Point(34, 166)
+        Me.btnPrevencion.Name = "btnPrevencion"
+        Me.btnPrevencion.Size = New System.Drawing.Size(635, 29)
+        Me.btnPrevencion.TabIndex = 3
+        Me.btnPrevencion.Text = "&Prevencion de Errores"
+        Me.btnPrevencion.UseVisualStyleBackColor = True
+        '
+        'btnEstetico
+        '
+        Me.btnEstetico.Location = New System.Drawing.Point(34, 212)
+        Me.btnEstetico.Name = "btnEstetico"
+        Me.btnEstetico.Size = New System.Drawing.Size(635, 29)
+        Me.btnEstetico.TabIndex = 4
+        Me.btnEstetico.Text = "&Diseño Estético y Minimalista"
+        Me.btnEstetico.UseVisualStyleBackColor = True
+        '
+        'btnControl
+        '
+        Me.btnControl.Location = New System.Drawing.Point(34, 265)
+        Me.btnControl.Name = "btnControl"
+        Me.btnControl.Size = New System.Drawing.Size(635, 29)
+        Me.btnControl.TabIndex = 5
+        Me.btnControl.Text = "&Control y Libertad de Ususario"
+        Me.btnControl.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(34, 308)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(595, 31)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Relación con  el Modelo de Procesamiento del Usuario"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtTexto
+        '
+        Me.txtTexto.Location = New System.Drawing.Point(34, 351)
+        Me.txtTexto.Multiline = True
+        Me.txtTexto.Name = "txtTexto"
+        Me.txtTexto.ReadOnly = True
+        Me.txtTexto.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtTexto.Size = New System.Drawing.Size(635, 175)
+        Me.txtTexto.TabIndex = 7
+        Me.txtTexto.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(34, 545)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(238, 51)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Busca una relación con una palabra clave o texto:"
+        '
+        'txtPalabra
+        '
+        Me.txtPalabra.Location = New System.Drawing.Point(269, 545)
+        Me.txtPalabra.Name = "txtPalabra"
+        Me.txtPalabra.Size = New System.Drawing.Size(264, 27)
+        Me.txtPalabra.TabIndex = 9
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(539, 543)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(130, 29)
+        Me.btnBuscar.TabIndex = 10
+        Me.btnBuscar.Text = "&Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'frmPrincipal
+        '
+        Me.AcceptButton = Me.btnBuscar
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(706, 655)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.txtPalabra)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtTexto)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnControl)
+        Me.Controls.Add(Me.btnEstetico)
+        Me.Controls.Add(Me.btnPrevencion)
+        Me.Controls.Add(Me.btnVisibilidad)
+        Me.Controls.Add(Me.btnReconocer)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Asistente de Heusísticas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnReconocer As Button
+    Friend WithEvents btnVisibilidad As Button
+    Friend WithEvents btnPrevencion As Button
+    Friend WithEvents btnEstetico As Button
+    Friend WithEvents btnControl As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtTexto As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPalabra As TextBox
+    Friend WithEvents btnBuscar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class frmPrincipal
+    'Autor:Alejandro Gonzalez Zarate
+#Region "Atributos"
+    'Matricula de estudiante 620115859
+    'Termina en 59->Impar
+    'Private Posicion As Integer
+    Dim componenteIPM() As String = {"Para los usuarios, es mucho más fácil reconocer lo que tienen que hacer, cuando el entorno provee pistas. Los usuarios perciben las pistas, las envían a la memoria de trabajo y esta les recuerda a los usuarios sobre el conocimiento que tienen almacenado en la memoria de largo plazo.",
+        "Si la computadora, ya sea de manera visual o auditiva, no provee a los usuarios con información de su procesamiento, no tendrán el conocimiento necesario para comprender lo que esta pasando ni las funciones que está realizando el sistema.",
+        "Hay varios motivos que hacen que el usuario cometa errores de percepción, deficiencia en la comprensión de lo que deben hacer, recuerdo de un aspecto de un comando en vez de recordar la sintaxis precisa—o en ocasiones cometen un error de dedo. Algunos de estos errores se pueden prevenir de la siguiente manera, que la interfaz muestre solamente las acciones que se puedan realizar en un momento dado (por ejemplo, sombreando los botones que no generan una acción válida en cierto momento). Otro tipo de errores se pueden detectar al momento que el usuario los comete ( por ejemplo, cuando una interfaz no permite que se utilice una abreviación incorrecta de un estado de la república en una dirección).",
+        "Esta heurística está relacionada con el aspecto de búsqueda visual de la percepción y memoria. Entre más amontonada esté la pantalla, el usuario batallará más para encontrar la información deseada. Además, entre más información llegue a la memoria de trabajo a través de la percepción de la búsqueda visual, más trabajo tendrá la memoria de largo plazo para inferir que información es relevante y cual no.",
+        "Los usuarios cometerán errores; así pues, necesitan una forma sencilla para recuperarse de estos errores."}
+#End Region
+
+#Region "Metodos"
+    ''' <summary>
+    ''' Asigna un IPM al cuadro de texto
+    ''' </summary>
+    ''' <param name="valor"></param>
+    Private Sub AsignarIPM(ByVal valor As Integer)
+        Dim indice As Integer = valor
+        Try
+            'Seleccion de texto
+            Select Case indice
+                Case 0
+                    txtTexto.Text = componenteIPM(0)
+                Case 1
+                    txtTexto.Text = componenteIPM(1)
+                Case 2
+                    txtTexto.Text = componenteIPM(2)
+                Case 3
+                    txtTexto.Text = componenteIPM(3)
+                Case 4
+                    txtTexto.Text = componenteIPM(4)
+            End Select
+        Catch
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Busca la primera coincidencia de un String de 3 caracteres o más, en todos los textos IPM
+    ''' </summary>
+    ''' <param name="palabra"></param>
+    Private Sub Buscar(ByVal palabra As String)
+        'Atributos
+        Dim indice As Integer = 0
+        Dim texto As String = ""
+        Dim mensaje, estilo, titulo As String
+        Dim respuesta As Integer = 0
+
+        Try
+            'Si no tengo texto en el txtPalabra
+            If String.IsNullOrEmpty(palabra) Then
+                Throw New ArgumentException
+
+                'Si mi longitud de palabra es < a 3
+            ElseIf palabra.Length < 3 Then
+                mensaje = "Por favor ingrese un String que sea de 3 caracteres o más"
+                estilo = vbInformation
+                titulo = "String invalido"
+                respuesta = MsgBox(mensaje, estilo, titulo)
+                txtTexto.Text = "Por favor ingrese un String que sea de 3 caracteres o más"
+                'Entra a buscar la palabra  
+            Else
+                mensaje = "Solamente se presenta la primera relación encontrada que contiene la palabra clave. Por lo tanto, se recomienda que las palabras claves sean tan auténticas como sea posible."
+                estilo = vbInformation
+                titulo = "Solo la primera"
+
+                respuesta = MsgBox(mensaje, estilo, titulo)
+                'Recorre en todos los arreglos
+                While indice < 5
+                    'Evalua si encuentra ocurrencia en un texto
+                    If componenteIPM(indice).IndexOf(palabra, 0) <> -1 Then
+                        Select Case indice
+                            Case 0
+                                texto = "Reconocer en vez de recordar:" + Environment.NewLine + Environment.NewLine
+                            Case 1
+                                texto = "Visibilidad de Estado del Sistema:" + Environment.NewLine + Environment.NewLine
+                            Case 2
+                                texto = "Prevencion de Errores:" + Environment.NewLine + Environment.NewLine
+                            Case 3
+                                texto = "Diseño Estetico y Minimalista:" + Environment.NewLine + Environment.NewLine
+                            Case 4
+                                texto = "Control y Libertad de Usuario:" + Environment.NewLine + Environment.NewLine
+                        End Select
+                        'Muestro texto y me salgo
+                        txtTexto.Text = texto + componenteIPM(indice)
+                        Exit While
+                    Else
+                        txtTexto.Text = "No se encontraron coincidencias"
+                    End If
+                    'Aumento indice para buscar en el siguiente
+                    indice = indice + 1
+                End While
+                'Si no encuentra coincidencia
+
+            End If
+        Catch ex As ArgumentException
+            mensaje = "Por favor ingrese un String de busqueda valido"
+            estilo = vbInformation
+            titulo = "String invalido"
+            respuesta = MsgBox(mensaje, estilo, titulo)
+            txtTexto.Text = "Por favor ingrese un String de busqueda valido"
+
+
+        End Try
+    End Sub
+#End Region
+
+#Region "Eventos"
+    Private Sub btnReconocer_Click(sender As Object, e As EventArgs) Handles btnReconocer.Click
+        AsignarIPM(0)
+    End Sub
+    Private Sub btnVisibilidad_Click(sender As Object, e As EventArgs) Handles btnVisibilidad.Click
+        AsignarIPM(1)
+    End Sub
+
+    Private Sub btnPrevencion_Click(sender As Object, e As EventArgs) Handles btnPrevencion.Click
+        AsignarIPM(2)
+    End Sub
+
+    Private Sub btnEstetico_Click(sender As Object, e As EventArgs) Handles btnEstetico.Click
+        AsignarIPM(3)
+    End Sub
+
+    Private Sub btnControl_Click(sender As Object, e As EventArgs) Handles btnControl.Click
+        AsignarIPM(4)
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Buscar(txtPalabra.Text)
+    End Sub
+
+    Private Sub txtPalabra_TextChanged(sender As Object, e As EventArgs) Handles txtPalabra.TextChanged
+        txtTexto.Clear()
+    End Sub
+#End Region
+End Class
